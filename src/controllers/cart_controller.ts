8850a88a8161f6b08ef8e458cf14ce0854e8dceb
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-03-22 14:25:48 -0600

    getCart and updateCart controllers finished

--- src/controllers/cart_controller.ts
+++ src/controllers/cart_controller.ts
@@ -1,32 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 const db = require("../db");
 
-//create cart
+//add new cart item
 export const addItemToCart = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const {prod_id, quantity} = req.body;
+    const { prod_id, quantity } = req.body;
     const user_id = req.session.user?.id;
     const newCart = await db.query(
-      "INSERT INTO commerce.cart_items (prod_id, quantity, user_id) VALUES ($1, $2, $3) RETURNING *",
+      "INSERT INTO commerce.cart_items (prod_id, quantity, user_id) VALUES ($1, $2, $3) ON CONFLICT (user_id, prod_id) DO UPDATE SET quantity = $2 RETURNING *",
       [prod_id, quantity, user_id]
     );
     if (newCart.rows.length === 0) {
-      throw Error('could not update')
+      res.send('could not update cart');
     } else {
       req.session.user!.cart = 1;
       res.send(newCart.rows[0]);
     }
   } catch (err: any) {
-    return err.message
+    return err.message;
   }
 };
 //get cart items
 /**
- *I dont like this get user cart should not also update user cart.
  * @param req
  * @param res
  * @param next
@@ -38,46 +37,28 @@ export const getUserCart = async (
   next: NextFunction
 ) => {
   try {
-    console.log("getUserCart");
     const user_id = req.session.user?.id;
     const cart_items = await db.query(
-      "SELECT prod_id FROM commerce.cart_items WHERE user_id = $1",
+      `SELECT 
+	      product.name, 
+	      product.description, 
+	      product.price, 
+	      quantity,
+	      product.price * quantity AS total
+      FROM commerce.cart_items 
+      INNER JOIN commerce.product
+      ON commerce.product.id = commerce.cart_items.prod_id
+      WHERE user_id = $1;`,
       [user_id]
     );
     if (cart_items.rows.length !== 0) {
       req.session.user!.cart = 1;
-      res.send(cart_items.rows[0]);
-    } else { 
-      req.session.user!.cart = -1
-      res.send('your cart is empty')
+      res.send(cart_items.rows);
+    } else {
+      req.session.user!.cart = -1;
+      res.send("your cart is empty");
     }
   } catch (err: any) {
     res.send(err);
   }
-};
-//add new cart item
-
-//update cart quantity
-/**
- *
- * @param cart_items @type { rows: [] }
- * @param prod_id @type Number
- * @param quantity @type Number
- */
-export const updateUserCart = async (
-  cart_items: { rows: [] },
-  prod_id: Number,
-  quantity: Number
-) => {
-  try {
-    cart_items.rows.forEach(async (item: { prod_id: Number }) => {
-      if ((item.prod_id = prod_id)) {
-        const updatedCart = await db.query(
-          "UPDATE commerce.cart_items SET quantity = quantity + $1 WHERE prod_id = $2 RETURNING *",
-          [quantity, prod_id]
-        );
-        console.log(updatedCart.rows[0]);
-      }
-    });
-  } catch (err: any) {}
 };
--- src/routes/shopping_cart.ts
+++ src/routes/shopping_cart.ts
@@ -4,14 +4,7 @@ import {
   addItemToCart,
 } from "../controllers/cart_controller";
 
-// get /  --getCart
 router.get("/:id", getUserCart);
-//post add item to cart
 router.post("/:id", addItemToCart)
-
-// update /id: --updateCart quantity
-// router.put("/:id");
-
-//delete cart
 
 export default router;
