13c52cbf32fcbc364b40362d3b4a14d4058520b7
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-03-23 14:58:20 -0600

    tinkering with queries might use zapatos

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,5 +1,6 @@
 const db = require("../db");
 import { Response, Request } from "express";
+import { queries } from "../db/queries";
 import { profileBodyReq } from "../types";
 
 /**
@@ -45,19 +46,19 @@ export const profileSettings = async (req: Request, res: Response) => {
     const reqId = req.params.id;
     const user = await db.query(
       `SELECT 
-        commerce.user.id, 
-        commerce.user.first_name, 
-        commerce.user.last_name,
-        commerce.user.email_address,
-        commerce.user.phone,
-        commerce.user_address.address_line1,
-        commerce.user_address.address_line2,
-        commerce.user_address.city,
-        commerce.user_address.zip_code,
-        commerce.user_address.country_code,
-        commerce.user_payment.expires,
-        commerce.user_payment.provider
-        FROM commerce.user 
+      commerce.user.id, 
+      commerce.user.first_name, 
+      commerce.user.last_name,
+      commerce.user.email_address,
+      commerce.user.phone,
+      commerce.user_address.address_line1,
+      commerce.user_address.address_line2,
+      commerce.user_address.city,
+      commerce.user_address.zip_code,
+      commerce.user_address.country_code,
+      commerce.user_payment.expires,
+      commerce.user_payment.provider
+      FROM commerce.user 
         JOIN commerce.user_address
             ON commerce.user.id = commerce.user_address.user_id
         JOIN commerce.user_payment
@@ -75,15 +76,16 @@ export const profileSettings = async (req: Request, res: Response) => {
   }
 };
 
+
 //will nedd to create a switch statement for each user profile field...
 export const updateProfile = async (req: Request, res: Response) => {
   try {
     const user_id = req.params.id
     const profileInfo: profileBodyReq = req.body;
     const userField = Object.keys(profileInfo)[0];
-
-    const updatedProfile = await db.query(
-      `UPDATE commerce.user SET first_name = $1 WHERE id = $2`,
+      
+    const updatedProfile = await db.getClient(
+      queries.updateProfileName,
       [profileInfo[userField], user_id]
     );
     res.send(updatedProfile);
