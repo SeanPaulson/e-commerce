c6b8d26b2ac233d633f5fe6c72af7cde65a40e16
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-03-25 19:16:54 -0600

    savePoint

--- src/controllers/cart_controller.ts
+++ src/controllers/cart_controller.ts
@@ -39,16 +39,7 @@ export const getUserCart = async (
   try {
     const user_id = req.session.user?.id;
     const cart_items = await db.query(
-      `SELECT 
-	      product.name, 
-	      product.description, 
-	      product.price, 
-	      quantity,
-	      product.price * quantity AS total
-      FROM commerce.cart_items 
-      INNER JOIN commerce.product
-      ON commerce.product.id = commerce.cart_items.prod_id
-      WHERE user_id = $1;`,
+      "SELECT * FROM commerce.get_user_cart($1);",
       [user_id]
     );
     if (cart_items.rows.length !== 0) {
@@ -64,20 +55,13 @@ export const getUserCart = async (
 };
 
 export const checkoutCart = async (req: Request, res: Response) => {
-  const user_id = req.params.id;
-  const { product_id, quantity, total } = req.body;
+  try {
+    const user_id = req.params.id;
+  // const { product_id, quantity, total } = req.body;
 
   //create a insert into order_details table
-  const order_details_id = await db.getClient(`
-  INSERT INTO commerce.order_details (
-    user_id,
-    total
-  )
-  SELECT product.id, product.price * quantity AS total
-  FROM commerce.cart_items 
-  INNER JOIN commerce.product
-  ON commerce.product.id = commerce.cart_items.prod_id
-  WHERE user_id = $1; returning id`, [user_id]);
+  const order_details_id = await db.getClient("CALL commerce.checkout(1004) RETURNING *;", 
+    [user_id]);
   if (order_details_id.rows) {
     return res.send(order_details_id);
   }
@@ -87,4 +71,7 @@ export const checkoutCart = async (req: Request, res: Response) => {
   //confirm payment (successful table inserts in this case)
   //then delete users items from order_items table
   //res with successful order and confirmation code
+  }catch (err: any) {
+    res.send(err);
+  }
 }
