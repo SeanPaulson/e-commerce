0f2fcaaf0896fa29d6daba8f33f68359e0c0fb52
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-06-15 11:56:01 -0600

    sass config fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './scss/app.scss';
+import './app.module.scss';
 import Button from 'react-bootstrap/Button'
 
 function App() {
