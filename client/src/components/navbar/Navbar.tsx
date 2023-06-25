5f524fc5d99cc825caaf175ceacc1ea65128e064
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-06-25 10:12:03 -0600

    navbar mobile

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,22 +1,64 @@
 import Navbar from "react-bootstrap/Navbar";
-// import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
-// import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 import "./_navbar.scss";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import Container from "react-bootstrap/esm/Container";
+import Form from "react-bootstrap/esm/Form";
+import { Offcanvas } from "react-bootstrap";
 
 function NavbarComponent() {
   return (
-    <Navbar expand="sm">
-      <Col>
-        <Navbar.Brand href="#home" className="navLogo">
-          E-commerce
-        </Navbar.Brand>
-      </Col>
-      <Col sm={{span: 2}}>
-        <Image src="/person.svg" alt="profile" roundedCircle></Image>
-        <Image src="/cart.svg" alt="shopping cart" roundedCircle></Image>
-      </Col>
+    <Navbar expand="md">
+      <Navbar.Brand href="#home" className="navLogo ">
+        E-commerce
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Link>
+          <Image
+            className="ms-4"
+            src="/person.svg"
+            alt="profile"
+            roundedCircle
+          ></Image>
+          <Image
+            className="ms-4"
+            src="/cart.svg"
+            alt="shopping cart"
+            roundedCircle
+          ></Image>
+        </Nav.Link>
+      </Nav>
+      <Container className="justify-content-start">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-btn">
+          <Image src="/list.svg"></Image>
+          <Navbar.Offcanvas restoreFocus>
+            <Offcanvas.Header closeButton>
+              <h1>
+                <b>Shop By Category</b>
+              </h1>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              {/* <category nav/div> */}
+              <p>ldkjf</p>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Navbar.Toggle>
+        <Form className="flex-grow-1 me-4">
+          <Form.Group>
+            <Form.Control size="sm" type="text" placeholder="Search" />
+          </Form.Group>
+        </Form>
+      </Container>
+      <Container>
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Item>klsdjf</Nav.Item> {/* <category nav/div> */}
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
