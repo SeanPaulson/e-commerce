ff98869ee9fe03b7c0a3708f704e0e362533076f
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-09-07 10:11:45 -0600

    fixed useConvertMoneyToNumber bug

--- client/src/hooks/useConvertMoneyToNumber.tsx
+++ client/src/hooks/useConvertMoneyToNumber.tsx
@@ -2,7 +2,7 @@ import { CartItem } from "../utils/types";
 
 export default function useConvertMoneyToNumber(data: CartItem[] | string): string {
     if (data[0] && data instanceof Array) {
-        return data.map((item) => item.total.substring(1)).reduce((prev, current) => prev + current);
+        return data.map((item) => Number(item.total.substring(1).replace(/,/g, ""))).reduce((prev, current) => prev + current).toFixed(2);
     }
     else if (data instanceof String) {
         return Number(data.substring(1)).toFixed(2);
