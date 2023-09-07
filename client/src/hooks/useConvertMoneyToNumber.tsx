3a98863f25a929a33b367e2bd01cead74ce59e36
Sean Paulson <61070403+SeanPaulson@users.noreply.github.com>
2023-09-07 09:59:29 -0600

    fixed bug#10

--- client/src/hooks/useConvertMoneyToNumber.tsx
+++ client/src/hooks/useConvertMoneyToNumber.tsx
@@ -2,8 +2,7 @@ import { CartItem } from "../utils/types";
 
 export default function useConvertMoneyToNumber(data: CartItem[] | string): string {
     if (data[0] && data instanceof Array) {
-        //TODO bug NaN when total is over a 1000
-        return data.map((item) => Number(item.total.substring(1))).reduce((prev, current) => prev + current).toFixed(2);
+        return data.map((item) => item.total.substring(1)).reduce((prev, current) => prev + current);
     }
     else if (data instanceof String) {
         return Number(data.substring(1)).toFixed(2);
